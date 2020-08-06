adbb6fb571c2ac2c0cef0a89a6906ac000571790
jacksonp12 <jacksondemente@gmail.com>
2020-08-06 18:37:33 -0500

    interfaces

---
+++ producto_persona/IProducto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace producto_persona
+{
+    interface IProducto
+    {
+        string NombreDeDueno();
+        string CantidadDeProducto();
+        string llega();
+        string registro();
+        string MarcaDeProducto();
+        string NumeroDeProducto();
+        string CiudadDeDestino();
+        string DireccionDondeDelEnvio();
+        string MiNombre();
+        string MiApellido();
+        string MiCorreo();
+        string MiNumero();
+        string ProductoCodigo();
+        string ProductoMarca();
+        string TipoDeProducto();
+    }
+}
